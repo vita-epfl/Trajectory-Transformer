7a0530b6a27c5b95a7386d655642d419d2bcd030
Gojko <gojkoc54@gmail.com>
2022-04-23 04:59:53 +0200

    added the main script that trains and evaluates on trajnet++

---
+++ trajnet_scripts/trajnet_run.sh
@@ -0,0 +1,18 @@
+cd ..
+
+MODES=3
+
+# Run K-means clustering
+CUDA_VISIBLE_DEVICES=0 python kmeans_trajnetpp.py \
+    --dataset_name colfree_trajdata --obs 9 
+
+cp colfree_trajdata_1000_200000_scaleTrue_rotTrue/clusters.mat datasets/colfree_trajdata/
+
+# Train the model 
+CUDA_VISIBLE_DEVICES=1 python train_trajnetpp_quantizedTF.py \
+    --dataset_name colfree_trajdata --name colfree_trajdata \
+    --obs 9 --batch_size 128 --max_epoch 100
+
+# Evaluate on Trajnet++ 
+python -m trajnet_evaluator \
+    --dataset_name colfree_trajdata --write_only --modes ${MODES} 
